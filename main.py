13585733faae20f9cc3be4cba8d440237258911b
Максим Эрлих <max-mkpb@yandex.ru>
2023-07-22 12:16:22 +0300

    add working version vithout classes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 from graphic_arts.start_game_banner import run_screensaver
 
+
 def attack(char_name: str, char_class: str) -> str:
     """Выполняет атаку на основе класса персонажа.
 
@@ -130,4 +131,4 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-    
+
